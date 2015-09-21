d923c75038c2ace289444ab789bf58cf4e346d2a
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2015-09-21 02:13:25 +0200

    Add some Database Migrations
    Users Migration
    News Migration
    Subscribtions Migration

--- MSTC/database/migrations/2015_09_21_000329_create_news_table.php
+++ MSTC/database/migrations/2015_09_21_000329_create_news_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateNewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('news', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('usertype');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('body');
+            $table->string('status');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('news');
     }
 }
