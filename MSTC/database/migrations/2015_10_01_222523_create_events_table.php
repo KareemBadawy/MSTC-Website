ce36d62b0f7f92665d86a190b11acb616eeabbc6
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2015-10-02 00:26:47 +0200

    Add Events Migration

---
+++ MSTC/database/migrations/2015_10_01_222523_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('body');
+            $table->boolean('status');
+            $table->datetime('started_at');
+            $table->datetime('ended_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
