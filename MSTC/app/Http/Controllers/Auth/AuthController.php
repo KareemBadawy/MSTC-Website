b00c69a1c6c954f62d79f32787d52782562ea035
Hammam94 <gentle_engieer@hotmail.com>
2015-10-12 23:25:12 +0200

    add redirect path after login and logout and register

--- MSTC/app/Http/Controllers/Auth/AuthController.php
+++ MSTC/app/Http/Controllers/Auth/AuthController.php
@@ -22,7 +22,8 @@ class AuthController extends Controller
     */
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
-    protected $redirectPath = '/';
+    protected $redirectPath = '/member';
+    protected $redirectAfterLogout = "/";
     /**
      * Create a new authentication controller instance.
      *
--- MSTC/resources/views/auth/register.blade.php
+++ MSTC/resources/views/auth/register.blade.php
@@ -3,7 +3,8 @@
 
 @section ('content')
 
-<form method="POST" action="auth/register" class="form-group">
+<form method="POST" url="auth/register" class="form-group">
+
     {!! csrf_field() !!}
 
     <div class="form-control">
@@ -11,25 +12,25 @@
         <input type="text" name="username" value="{{ old('username') }}">
     </div>
 
-    <div>
+    <div class="form-control">
         Email
         <input type="email" name="email" value="{{ old('email') }}">
     </div>
-    <div>
+    <div class="form-control">
         Vertical
         <input type="text" name="vertical" value="{{ old('vertical') }}">
     </div>
-    <div>
+    <div class="form-control">
         Password
         <input type="password" name="password">
     </div>
 
-    <div>
+    <div class="form-control">
         Confirm Password
         <input type="password" name="password_confirmation">
     </div>
 
-    <div>
+    <div class="form-control">
         <input type="submit" value="Register Now">
     </div>
 </form>
